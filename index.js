570f3f58dfa5446805da710dd48b754716776ebd
Yi Heng <yihengwang2000@gmail.com>
2021-01-31 00:16:56 -0500

    Document the functions

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// generateDiceResult() generates a random number between 1 and 6 inclusive
 function generateDiceResult(){
   return Math.floor(1 + Math.random()*6);
 }
@@ -5,15 +6,20 @@ function generateDiceResult(){
 let dice1 = generateDiceResult();
 let dice2 = generateDiceResult();
 
+// loadDiceImage(imgNumber) loads the dice image associated with the number
+//   provided
 function loadDiceImg(imgNumber){
   return diceImg = "images/dice" + imgNumber + ".png"
 }
 
+// uploadDiceImg(dice1, dice2) uploads the dice images with query selectors
 function uploadDiceImg(dice1, dice2){
   document.querySelector("img.dice1").setAttribute("src", loadDiceImg(dice1));
   document.querySelector("img.dice2").setAttribute("src", loadDiceImg(dice2));
 }
 
+// winnerMessage(dice1, dice2) prints out a winning message depending on the
+//   results of the numbers shown on dice1 and dice2
 function winnerMessage(dice1, dice2){
   if (dice1 === dice2) {
     document.querySelector("h1").textContent = "Draw!";
